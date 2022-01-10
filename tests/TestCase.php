67e0545f6b79ca054276897fafc6d20e9d2f14c5
Attia Ahmed <attia@famcare.app>
2022-01-10 23:19:23 +0300

    init package

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AttiaAhmed\ExtendableAction\Tests;
+
+use Mockery;
+use AttiaAhmed\ExtendableAction\ExtendableAction;
+use AttiaAhmed\ExtendableAction\ExtendableActionBaseServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+    
+}
